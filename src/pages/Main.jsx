bf1644d2867d4cdb5cbbd8529ed7253ec799d747
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-07-08 11:02:27 -0300

    Feat: Pagina sobre - css e imagens

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Components/Header';
+import Sobre from './Sobre';
 
 function Main() {
   return (
     <div>
       <Header />
+      <Sobre />
     </div>
   );
 }

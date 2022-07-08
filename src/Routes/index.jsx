1b00539c9d816926f2291ad5a35195fd96ebbf4b
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-07-08 10:54:50 -0300

    Feat: Criando o header e uma pagina main

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Sobre from '../pages/Sobre';
 import Projetos from '../pages/Projetos';
 import Skills from '../pages/Skills';
+import Main from '../pages/Main';
 
 function Rotas() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Main />} />
         <Route path="/Sobre" element={<Sobre />} />
         <Route path="/Projetos" element={<Projetos />} />
         <Route path="/Skills" element={<Skills />} />
